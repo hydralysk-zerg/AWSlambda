2da8e51cd629b63c9d01b3e8368bbb60ce89d583
Alexey <aleksey.ilchenko.m@gmail.com>
2021-12-24 11:59:36 +0200

    start push AWS

--- app.py
+++ app.py
@@ -1,11 +1,95 @@
+from typing import KeysView
+import requests
+import json
 from chalice import Chalice
 
 app = Chalice(app_name='AWSlambda')
 
+# API_KEY1 = 'PKFU4FZT9Z8047JFEYZ0'
+# SECRET_KEY1 = 'vJ8pFUAvEl0kgnsuCSw8JMIWrofpiO12PJB4XkEN'
+# BASE_URL = "https://psotlpgw85.execute-api.us-east-2.amazonaws.com/api/"
+BASE_URL = "hhttp://127.0.0.1:8000"
+# ORDERS_URL = "{}/v2/orders".format(BASE_URL)
+# HEADERS = {'APCA-API-KEY-ID': API_KEY1, 'APCA-API-SECRET-KEY': SECRET_KEY1}
+
+data = {}
+
+
+@app.route('/buy_stock', methods=['POST'])
+def buy_stock():
+    request = app.current_request
+    webhook_message = request.json_body
+    # webhook_text.append(webhook_message)
+
+    print(webhook_message)
+    print(webhook_message.keys())
+    print(webhook_message['ETH'])
+    #
+
+    datas = {
+        'open': webhook_message['open'],
+        'high': webhook_message['high'],
+        'low': webhook_message['low'],
+        'close': webhook_message['close'],
+        'ticker': webhook_message['ticker'],
+        'ETH': {
+            'startShort': False,
+            'startLong': False,
+            'stopShort': False,
+            'stopLong': False,
+            'tp_percent': None,
+            'last_candle': 'null',
+            'sl_tries': 'null',
+            'isOn': False,
+            'LoseActive': 'null'
+        }
+    }
+
+    for key in datas:
+        data[key] = datas[key]
+        print(key, "->", datas[key])
+    # r = requests.post(BASE_URL, json=data)
+    # response = json.loads(r.content)
+    #
+
+    with open('json.json', 'r', encoding='utf-8') as read_json:
+        text = json.load(read_json)
+        if(text != webhook_message):
+            with open('json.json', 'w', encoding='utf-8') as outfile:
+                json.dump(webhook_message, outfile)
+                print("add")
+        else:
+            print("pass")
+
+    return {
+        'message': 'I bought the stock!',
+        'webhook_messagese': data,
+        # 'id': response['id'],
+
+        # 'client_order_id': response['client_order_id'],
+    }
+
 
 @app.route('/')
 def index():
-    return {'hello': 'world'}
+    return {
+        'Hello': 'world!',
+        'message': 'I bought the stock!',
+        'webhook_message': data
+    }
+# @app.route('/watchlist')
+# def watchlist():
+#     stream = {
+#         "TWTR": [
+#             "Twitter tweet 1", "Twitter tweet 2"
+#         ],
+#         "SQ": [
+#             "Square tweet 1", "Square tweet 2"
+#         ]
+#     }
+#     return {
+#         'stream': stream
+#     }
 
 
 # The view function above will return {"hello": "world"}
